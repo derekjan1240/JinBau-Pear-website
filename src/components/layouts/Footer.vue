32c83b7fc9603f6d50c0b99cab9c820674e4d934
Derek Zhan <derekjan1240@gmail.com>
2019-04-15 15:52:28 +0800

    social-media with v-for

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,11 +1,22 @@
 <template>
     <section id="footer" class="container-fluid">
         <GoogleMap/>
-        <div id="social-media">
+
+        <div id="social-media" class="pt-3">
+          <ul class="list-inline">
+            <li v-for="socialMediaList in socialMediaLists" :key="socialMediaList._id" class="list-inline-item">
+              <a :href="socialMediaList.link"><font-awesome-icon :icon="socialMediaList.icon" size="lg" class="m-3" :style="socialMediaList.styles"/></a>
+            </li>
+          </ul>
+          <p>©2019 by jinbau-pear.</p>
+        </div>
+
+        <!-- <div id="social-media">
             <a :href="facebook.link"><font-awesome-icon :icon="['fab', 'facebook']" size="lg" class="m-3" style="color: #3B5998"/></a>
             <font-awesome-icon :icon="['fab', 'line']" size="lg" class="m-3" style="color: 00c300"/>
             <p>©2019 by jinbau-pear.</p>
-        </div>
+        </div>  -->
+
     </section>
 </template>
 
@@ -21,11 +32,22 @@ export default {
     return {
       facebook:{
           link: 'https://www.facebook.com/%E9%87%91%E5%AF%B6%E7%9A%84%E9%AB%98%E6%8E%A5%E6%A2%A8-Jinbau-Pear-372478336933774/?modal=admin_todo_tour'
+      },
+      socialMediaLists:{
+        facebook:{
+          _id:0,
+          icon:['fab', 'facebook'],
+          styles:{color: '#3B5998'},
+          link: 'https://www.facebook.com/%E9%87%91%E5%AF%B6%E7%9A%84%E9%AB%98%E6%8E%A5%E6%A2%A8-Jinbau-Pear-372478336933774/?modal=admin_todo_tour'
+        },
+        line:{
+          _id:1,
+          icon:['fab', 'line'],
+          styles:{color: '#00c300'},          
+          link: 'https://www.facebook.com/%E9%87%91%E5%AF%B6%E7%9A%84%E9%AB%98%E6%8E%A5%E6%A2%A8-Jinbau-Pear-372478336933774/?modal=admin_todo_tour'
+        }
       }
 
-    //   contactStyle: {
-    //     paddingRight: '8px'
-    //   },
     //   designer: `
     //     <span style="font-size:0.9em">Designed by
     //       <span style="color: #e27575;font-size: 14px;">❤</span>
